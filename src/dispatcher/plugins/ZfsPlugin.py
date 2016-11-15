c68ad88515f9f4dcfa547337b4f1b2cca7be0038
Jakub Klama <jakub@ixsystems.com>
2016-11-15 15:00:24 -0800

    Adjust to changed zfs.receive() prototype

--- src/dispatcher/plugins/ZfsPlugin.py
+++ src/dispatcher/plugins/ZfsPlugin.py
@@ -1241,7 +1241,7 @@ class ZfsReceiveTask(Task):
     def run(self, name, fd, force=False, nomount=False, props=None, limitds=None):
         try:
             zfs = get_zfs()
-            zfs.receive(name, fd.fd, force, nomount, props, limitds)
+            zfs.receive(name, fd.fd, force, nomount, False, props, limitds)
         except libzfs.ZFSException as err:
             raise TaskException(zfs_error_to_errno(err.code), str(err))
         finally:
