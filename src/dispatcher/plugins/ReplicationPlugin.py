c7ba61e8c0c0c2d53f22cad9a6a36dfcb9d92448
Jakub Klama <jakub@ixsystems.com>
2016-11-14 23:38:19 -0800

    Exclude mountpoint property in received datasets.

--- src/dispatcher/plugins/ReplicationPlugin.py
+++ src/dispatcher/plugins/ReplicationPlugin.py
@@ -1354,7 +1354,8 @@ class ReplicateDatasetTask(ProgressTask):
                             'receive_properties': {
                                 'name': action['remotefs'],
                                 'force': force,
-                                'nomount': nomount
+                                'nomount': nomount,
+                                'props': {'mountpoint': True}
                             },
                             'estimated_size': send_size
                         }
