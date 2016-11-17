5e2a21bb6b7f787f47afad24c5ac9fc7acf647b0
650elx <bprokop02@gmail.com>
2016-11-16 17:24:19 -0800

    Omit the BOOTLOADER state and mark is as RUNNING

--- src/dispatcher/plugins/DomainControllerPlugin.py
+++ src/dispatcher/plugins/DomainControllerPlugin.py
@@ -55,10 +55,10 @@ class DCProvider(Provider):
             [('id', '=', dc_vm['vm_id'])],
             {'select': 'status.state', 'single': True}
         )
-        if state != 'RUNNING':
+        if state not in ('RUNNING', 'BOOTLOADER'):
             raise RpcException(errno.ENOENT, "Domain Controller service is not running")
         else:
-            return state
+            return 'RUNNING'
 
     def service_stop(self):
         dc_vm = self.get_config()
