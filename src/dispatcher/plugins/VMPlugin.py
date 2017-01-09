95accb4a07c30de4c0e05e2123c8067dfe2cbb27
Wojciech Kloska <shangteus@ixsystems.com>
2017-01-09 20:55:31 +0100

    UPDATED: make error on `vm delete` with clones more descriptive

--- src/dispatcher/plugins/VMPlugin.py
+++ src/dispatcher/plugins/VMPlugin.py
@@ -1062,8 +1062,12 @@ class VMDeleteTask(Task):
 
         vm = self.datastore.get_by_id('vms', id)
 
-        if self.datastore.exists('vms', ('parent', '=', id)):
-            raise TaskException(errno.EACCES, 'Cannot delete VM {0}. VM has clones'.format(vm['name']))
+        clones = list(self.datastore.exists('vms', ('parent', '=', id), select='name'))
+        if clones:
+            raise TaskException(
+                errno.EACCES,
+                'Cannot delete VM {0}. VM has clones: '.format(vm['name']) + ' '.join(clones)
+            )
 
         try:
             delete_config(
