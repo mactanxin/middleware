3e73cc605092247845fe60826878cfcb0f898629
Suraj Ravichandran <suraj@ixsystems.com>
2017-01-09 03:49:27 -0800

    Fix patch path

--- nas_ports/freenas/freenas-migrate93/files/patch-django_utils_html__parser.py
+++ nas_ports/freenas/freenas-migrate93/files/patch-django_utils_html__parser.py
@@ -1,5 +1,5 @@
---- django/utils/html_parser.py      2017-01-08 23:54:49.046133025 -0800
-+++ django/utils/html_parser.py.orig 2017-01-08 23:45:45.749619996 -0800
+--- django1611/django/utils/html_parser.py      2017-01-08 23:54:49.046133025 -0800
++++ django1611/django/utils/html_parser.py.orig 2017-01-08 23:45:45.749619996 -0800
 @@ -9,12 +9,7 @@
      (current_version >= (3, 0) and current_version < (3, 2, 3))
  )
