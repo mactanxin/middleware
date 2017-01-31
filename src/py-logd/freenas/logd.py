a52f26638c3b5d9f2f10616166745eec9c8f971d
Jakub Klama <jakub@ixsystems.com>
2017-01-31 16:43:26 +0100

    Make logging timeout less restrictive

--- src/py-logd/freenas/logd.py
+++ src/py-logd/freenas/logd.py
@@ -70,7 +70,7 @@ class LogdLogHandler(logging.Handler):
         if record.exc_info:
             item['exception'] = '\n'.join(traceback.format_exception(*record.exc_info))
 
-        self.client.call_sync('logd.logging.push', item, timeout=5)
+        self.client.call_sync('logd.logging.push', item, timeout=20)
 
     def close(self):
         super(LogdLogHandler, self).close()
