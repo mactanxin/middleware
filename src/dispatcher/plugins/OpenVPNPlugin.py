1ae3ca920e01661d22df5bbf7817f368c7c1befc
650elx <bprokop02@gmail.com>
2017-02-18 02:37:37 +0100

    Wrong indentation

--- src/dispatcher/plugins/OpenVPNPlugin.py
+++ src/dispatcher/plugins/OpenVPNPlugin.py
@@ -148,7 +148,7 @@ class OpenVpnConfigureTask(Task):
             )
 
             if not ca_cert:
-                    raise TaskException(errno.EINVAL, 'Provided CA certificate does not exist in config database.')
+                raise TaskException(errno.EINVAL, 'Provided CA certificate does not exist in config database.')
 
             else:
                 openvpn_updated['ca'] = ca_cert
