5c8f074051c18df3c830b314db3c1111a2cbe7e9
Suraj Ravichandran <suraj@ixsystems.com>
2017-02-21 06:14:05 -0800

    Check for password to be an instance of dispatcher.Password before assuming it is so.

--- src/dispatcher/plugins/WebDAVPlugin.py
+++ src/dispatcher/plugins/WebDAVPlugin.py
@@ -64,7 +64,7 @@ class WebDAVConfigureTask(Task):
 
     def run(self, webdav):
         node = ConfigNode('service.webdav', self.configstore).__getstate__()
-        if 'password' in webdav:
+        if isinstance(webdav.get('password'), Password):
             webdav['password'] = webdav['password'].secret
 
         for p in ('http_port', 'https_port'):
