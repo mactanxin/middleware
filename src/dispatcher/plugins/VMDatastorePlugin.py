8be707b336cf1505ced1fe710fa8b99eaf1d69f6
Wojciech Kloska <shangteus@ixsystems.com>
2017-02-24 19:56:32 +0100

    FIXED: avoid circular dependencies by explicitly specifying datastore type

--- src/dispatcher/plugins/VMDatastorePlugin.py
+++ src/dispatcher/plugins/VMDatastorePlugin.py
@@ -42,7 +42,7 @@ class DatastoreProvider(Provider):
 
         def extend(obj):
             obj['capabilities'] = drivers[obj['type']]
-            obj['state'] = self.dispatcher.call_sync('vm.datastore.get_state', obj['id'])
+            obj['state'] = self.dispatcher.call_sync(f'vm.datastore.{obj["type"]}.get_state', obj['id'])
             return obj
 
         def doit():
